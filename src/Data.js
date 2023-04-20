9b7b8d7f79fbb59e98fbcb67c1669e69a752743d
suhas <suhas@plainsurf.com>
2023-04-20 14:21:14 +0530

    initial commit

---
+++ src/Data.js
@@ -0,0 +1,139 @@
+const data = [
+    {
+        question: "What is the capital of France?",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Madrid", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false }
+        ]
+    },
+    {
+        question: "What is the largest planet in our solar system?",
+        answers: [
+            { text: "Venus", correct: false },
+            { text: "Jupiter", correct: true },
+            { text: "Mars", correct: false },
+            { text: "Mercury", correct: false }
+        ]
+    },
+    {
+        question: "Who painted the Mona Lisa?",
+        answers: [
+            { text: "Vincent van Gogh", correct: false },
+            { text: "Claude Monet", correct: false },
+            { text: "Pablo Picasso", correct: false },
+            { text: "Leonardo da Vinci", correct: true }
+        ]
+    },
+    {
+        question: "What is the highest mountain in the world?",
+        answers: [
+            { text: "Mount Kilimanjaro", correct: false },
+            { text: "Mount Everest", correct: true },
+            { text: "Mount Fuji", correct: false },
+            { text: "Mount Denali", correct: false }
+        ]
+    },
+    {
+        question: "What is the name of the first man on the moon?",
+        answers: [
+            { text: "Buzz Aldrin", correct: false },
+            { text: "Neil Armstrong", correct: true },
+            { text: "Yuri Gagarin", correct: false },
+            { text: "John Glenn", correct: false }
+        ]
+    },
+    {
+        question: "What is the largest ocean in the world?",
+        answers: [
+            { text: "Arctic Ocean", correct: false },
+            { text: "Indian Ocean", correct: false },
+            { text: "Atlantic Ocean", correct: false },
+            { text: "Pacific Ocean", correct: true }
+        ]
+    },
+    {
+        question: "Who invented the telephone?",
+        answers: [
+            { text: "Alexander Graham Bell", correct: true },
+            { text: "Thomas Edison", correct: false },
+            { text: "Nikola Tesla", correct: false },
+            { text: "Guglielmo Marconi", correct: false }
+        ]
+    },
+    {
+        question: "What is the smallest country in the world?",
+        answers: [
+            { text: "Monaco", correct: false },
+            { text: "Vatican City", correct: true },
+            { text: "San Marino", correct: false },
+            { text: "Liechtenstein", correct: false }
+        ]
+    },
+    {
+        question: "Who wrote the Harry Potter series of books?",
+        answers: [
+            { text: "J.K. Rowling", correct: true },
+            { text: "Stephenie Meyer", correct: false },
+            { text: "Suzanne Collins", correct: false },
+            { text: "Veronica Roth", correct: false }
+        ]
+    },
+    {
+        question: "Which city is known as the 'Eternal City'?",
+        answers: [
+            { text: "Athens", correct: false },
+            { text: "Rome", correct: true },
+            { text: "Jerusalem", correct: false },
+            { text: "Istanbul", correct: false }
+        ]
+    },
+    {
+        question: "What is the largest mammal in the world?",
+        answers: [
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+            { text: "Whale", correct: true },
+            { text: "Hippopotamus", correct: false }
+        ]
+    },
+    {
+        question: "What is the chemical symbol for gold?",
+        answers: [
+            { text: "Go", correct: false },
+            { text: "Au", correct: true },
+            { text: "Ag", correct: false },
+            { text: "Cu", correct: false }
+        ]
+    },
+    {
+        question: "What is the largest bird in the world?",
+        answers: [
+            { text: "Ostrich", correct: true },
+            { text: "Eagle", correct: false },
+            { text: "Emu", correct: false },
+            { text: "Albatross", correct: false }
+        ]
+    },
+    {
+        question: "What is the chemical formula for water?",
+        answers: [
+            { text: "H2S", correct: false },
+            { text: "CO2", correct: false },
+            { text: "H2O", correct: true },
+            { text: "NaCl", correct: false }
+        ]
+    },
+    {
+        question: "Which country is known as the 'Land of the Rising Sun'?",
+        answers: [
+            { text: "China", correct: false },
+            { text: "Japan", correct: true },
+            { text: "Korea", correct: false },
+            { text: "Vietnam", correct: false }
+        ]
+    }
+]
+
+export default data;
