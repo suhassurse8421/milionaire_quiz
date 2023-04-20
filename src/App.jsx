2fed1a813e1ce7e42f77921a9c6a2e2bd4d4cc42
suhas <suhas@plainsurf.com>
2023-04-20 15:03:18 +0530

    added sound effect to game

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./app.css"
 import Trivia from "./components/Trivia";
 import data from "./Data";
+import Timer from "./components/Timer";
 
 
 function App() {
@@ -10,23 +11,32 @@ function App() {
   const [stop, setStop] = useState(false);
   const [earned, setEarned] = useState("$ 0");
 
-  const moneyPyramid = [
-    { id: 1, amount: "$ 100" },
-    { id: 2, amount: "$ 200" },
-    { id: 3, amount: "$ 300" },
-    { id: 4, amount: "$ 500" },
-    { id: 5, amount: "$ 1000" },
-    { id: 6, amount: "$ 2000" },
-    { id: 7, amount: "$ 4000" },
-    { id: 8, amount: "$ 8000" },
-    { id: 9, amount: "$ 16000" },
-    { id: 10, amount: "$ 32000" },
-    { id: 11, amount: "$ 64000" },
-    { id: 12, amount: "$ 125000" },
-    { id: 13, amount: "$ 250000" },
-    { id: 14, amount: "$ 500000" },
-    { id: 15, amount: "$ 1000000" },
-  ].reverse();
+  const moneyPyramid = useMemo(() =>
+    [
+      { id: 1, amount: "$ 100" },
+      { id: 2, amount: "$ 200" },
+      { id: 3, amount: "$ 300" },
+      { id: 4, amount: "$ 500" },
+      { id: 5, amount: "$ 1000" },
+      { id: 6, amount: "$ 2000" },
+      { id: 7, amount: "$ 4000" },
+      { id: 8, amount: "$ 8000" },
+      { id: 9, amount: "$ 16000" },
+      { id: 10, amount: "$ 32000" },
+      { id: 11, amount: "$ 64000" },
+      { id: 12, amount: "$ 125000" },
+      { id: 13, amount: "$ 250000" },
+      { id: 14, amount: "$ 500000" },
+      { id: 15, amount: "$ 1000000" },
+    ].reverse(),
+    []
+  );
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [moneyPyramid, questionNumber])
+
 
   return (
     <div className="app">
@@ -34,7 +44,9 @@ function App() {
         {stop ? (<h1 className="endText">You Earned: {earned}</h1>) : (
           <>
             <div className="top">
-              <div className="timer">30</div>
+              <div className="timer">
+                <Timer setStop={setStop} questionNumber={questionNumber} />
+              </div>
             </div>
             <div className="bottom">
               <Trivia
--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react"
+import useSound from "use-sound";
+import play from "../assets/play.mp3"
+import correct from "../assets/correct.mp3"
+import wrong from "../assets/wrong.mp3"
 
 export default function Trivia({
     data,
@@ -10,7 +14,13 @@ export default function Trivia({
     const [question, setQuestion] = useState(null);
     const [selectedAnswer, setSelectedAnswer] = useState(null);
     const [className, setClassName] = useState("answer");
+    const [letsPlay] = useSound(play);
+    const [correctAnswer] = useSound(correct);
+    const [wrongAnswer] = useSound(wrong);
 
+    useEffect(() => {
+        letsPlay();
+    }, [letsPlay]);
 
     useEffect(() => {
         setQuestion(data[questionNumber - 1]);
@@ -28,12 +38,18 @@ export default function Trivia({
         delay(300, () =>
             setClassName(a.correct ? "answer correct" : "answer wrong")
         );
-        delay(6000, () => {
+        delay(5000, () => {
             if (a.correct) {
-                setQuestionNumber((prev) => prev + 1);
-                setSelectedAnswer(null);
+                correctAnswer();
+                delay(1000, () => {
+                    setQuestionNumber((prev) => prev + 1);
+                    setSelectedAnswer(null);
+                })
             } else {
-                setStop(true);
+                wrongAnswer();
+                delay(1000, () => {
+                    setStop(true);
+                })
             }
         })
     }
